df2b6e8aef8824bcc8cf45c3122c8de85d4a2998
crawler995 <zhang_995@foxmail.com>
2020-08-04 16:29:07 +0800

    feat: 新增许多组件，基本覆盖了常用组件

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,18 @@ import {
   Text, 
   Image, 
   Textarea,
-  Form
+  Form,
+  Icon,
+  Progress,
+  Checkbox,
+  CheckboxGroup,
+  RadioGroup,
+  Radio,
+  Label,
+  Slider,
+  Switch,
+  Picker,
+  ScrollView
 } from '@tarojs/components';
 import ComponentsContainer from './components/utils/components-container';
 
@@ -17,9 +28,17 @@ function App() {
       <ComponentsContainer componentsName="View">
         <View>hello world</View>
       </ComponentsContainer>
+
+      <ComponentsContainer componentsName="ScrollView">
+        <ScrollView style={{ height: '200px' }} scrollY scrollWithAnimation>
+          <View style={{ height: '150px' }}>ScrollViewItem</View>
+          <View style={{ height: '150px' }}>ScrollViewItem</View>
+          <View style={{ height: '150px' }}>ScrollViewItem</View>
+        </ScrollView>
+      </ComponentsContainer>
       
-      <ComponentsContainer componentsName="Button">
-        <Button>hello world</Button>
+      <ComponentsContainer componentsName="Button & Form">
+        <Button onClick={() => console.log('hello world')}>hello world</Button>
         <Button type="warn">hello world</Button>
         <Button size="mini">hello world</Button>
         <Button type="warn" size="mini">hello world</Button>
@@ -56,6 +75,57 @@ function App() {
         <Textarea placeholder="maxlength=6" maxlength={6} />
         <Textarea placeholder="with onInput callback" onInput={(e: any) => console.log(e.detail.value)} />
         <Textarea placeholder="with onFocus callback" onFocus={() => console.log('textarea focused')} />
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Icon">
+        <Icon size="30" type="success" />
+        <Icon size="45" type="info" />
+        <Icon size="60" type="warn" color="#ccc" />
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Progress">
+        <Progress percent={20} showInfo strokeWidth={2} />
+        <Progress percent={40} strokeWidth={2} active />
+        <Progress percent={60} strokeWidth={2} active />
+        <Progress percent={80} strokeWidth={2} active activeColor="blue" />
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Checkbox & Label">
+        <CheckboxGroup onChange={(e: any) => console.log(e.detail.value)}>
+          <Checkbox value='选项1' checked id="checkbox-1" />
+          <Label for="checkbox-1">选项1</Label>
+          <Checkbox id="checkbox-2" value='选项2'/>
+          <Label for="checkbox-2">选项2</Label>
+        </CheckboxGroup>
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Radio & Label">
+        <RadioGroup onChange={(e: any) => console.log(e.detail.value)}>
+          <Radio value='选项1' checked id="radio-1" />
+          <Label for="radio-1">选项1</Label>
+          <Radio value='选项2' id="radio-2" />
+          <Label for="radio-2">选项1</Label>
+        </RadioGroup>
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Slider">
+        <Slider step={1} value={50} onChange={(e) => console.log(e.detail.value)} />
+        <Slider step={1} value={50} showValue onChange={(e) => console.log(e.detail.value)} />
+        <Slider step={1} value={100} showValue min={50} max={200} onChange={(e) => console.log(e.detail.value)}/>
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Switch">
+        <Switch onChange={() => console.log('switch changed')} />
+        <Switch checked onChange={() => console.log('switch changed')} />
+      </ComponentsContainer>
+
+      <ComponentsContainer componentsName="Picker">
+        <Picker mode="time" onChange={(e) => console.log(e.detail.value)} value="00:00">
+          <View>Time Picker | Click me!</View>
+        </Picker>
+        <Picker mode="date" onChange={(e) => console.log(e.detail.value)} value="2020-08-04">
+          <View>Date Picker | Click me!</View>
+        </Picker>
       </ComponentsContainer>
     </div>
   );
